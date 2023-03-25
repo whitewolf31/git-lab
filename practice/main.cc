b8506163698627dd13737861991ef9e4876f73f9
Cristian Vijelie <cristianvijelie@gmail.com>
2023-03-25 11:15:05 +0200

    Add basic implementation of the program

---
+++ practice/main.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout << "Please enter a number" << endl;
+    cin >> n;
+    cout << "Your number + 5 is: " << n + 5 << endl;
+
+    return 0;
+}
